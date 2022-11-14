9d0912ecd3d25fd7b09b4fa0a38cd74cbb548771
wjnb11 <118050050+wjnb11@users.noreply.github.com>
2022-11-14 19:54:45 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,121 @@
+
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffledQuestions,currentQuestionIndex;
+let quizScore =0;
+
+
+startButton.addEventListener('click',startGame)
+
+nextButton.addEventListener('click' ,() =>{
+    currentQuestionIndex++
+    setnextQuestion()
+})
+
+
+function startGame(){
+    startButton.classList.add('hide')
+    shuffledQuestions=questions.sort(() =>Math.random() -0.5)
+    currentQuestionIndex=0;
+    questionContainerElement.classList.remove('hide')
+    setnextQuestion()
+    quizScore=0
+}
+
+function setnextQuestion() {
+    resetState();
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+    questionsElement.question =question.question;
+    question.answers.forEach((answer) =>{
+        const button =document.createElement('button')
+        button.innerText=answer.text;
+        button.classList.add('btn')
+        if(answer.correct) {
+            button.dataset.correct =answer.correct
+        }
+        button.addEventListener('click',selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
+}
+
+
+function resetState(){
+    clearStatusClass(document.body)
+    nextButton.classList.add('hide')
+    while(answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
+}
+
+function selectAnswer(e){
+    const selectedButton=e.target
+    const correct =selectedButton.dataset.correct
+
+    setStatusClass(document.body,correct)
+    Array.from(answerButtonsElement.children).forEach((button)=>{
+        setStatusClass(button,button.dataset.correct)
+    })
+    if(shuffledQuestions.length >currentQuestionIndex +1){
+        nextButton.classList.remove("hide")
+    }else {
+        startButton.innerText ="restart"
+        startButton.classList.remove("hide")
+    }
+    if(selectedButton.dataset = correct){
+        quizScore++
+    }
+    document.getElementById('right-answers').innerText=quizScore
+}
+
+function setStatusClass(element,correct){
+    clearStatusClass(element)
+    if(correct){
+        element.classList.add("correct")
+    }else {
+        element.classList.add("wrong")
+    }
+}
+
+
+
+function clearStatusClass(element){
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
+const questions =[
+    {
+        question: 'What is the name of the cat of Sebastien Stordeur?',
+        answers : [
+            { text: 'Sabina', correct: false},
+            { text: 'Fiona', correct: false},
+            { text: 'Zoubie', correct: true},
+            { text: 'Lou', correct: false}
+        ],
+    },
+    {
+        question: 'What is the nationality of the girlfriend of Sebastien Stordeur?',
+        answers : [
+            { text: 'Spanish', correct: false},
+            { text: 'Filipino', correct: true},
+            { text: 'Indian', correct: false},
+            { text: 'Thai', correct: false},
+        ],
+    },
+    {
+        question: 'What is 3 + 2?',
+        answers : [
+            { text: '5', correct: true},
+            { text: '6', correct: false},
+            { text: '8', correct: false},
+            { text: '7', correct: false},
+        ],
+    }
+]
